91eb4691a8bcce19655edabebace4946446f77ad
RitaJ <ritaj777@gmail.com>
2018-11-09 14:17:41 +0200

    added FosUser bundle

---
+++ src/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritaj
+ * Date: 18.11.9
+ * Time: 13.36
+ */
+
+namespace App\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
