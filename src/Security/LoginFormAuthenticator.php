39ea411804611e3115776a351792e11851c1f005
RitaJ <ritaj777@gmail.com>
2018-11-19 11:46:15 +0200

    working email

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,8 +85,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->router->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->router->generate('home'));
+
     }
 
     protected function getLoginUrl()
