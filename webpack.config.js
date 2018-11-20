fbdfe9528869f1908b8102e826aa48746dc72e9f
viktorasj <viktoras.jefimovas@gmail.com>
2018-11-20 12:40:19 +0200

    some FE

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ Encore
      */
     .addEntry('js/app', './assets/js/app.js')
     .addStyleEntry('css/app', './assets/css/app.scss')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('js/main', './assets/js/main.js')
+    .addEntry('libs/mapoid', './assets/libs/mapoid.js')
 
     /*
      * FEATURE CONFIG
