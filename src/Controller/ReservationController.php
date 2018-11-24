b956b36c4c014f34833d4afad4363ad1a3a8e52a
RitaJ <ritaj777@gmail.com>
2018-11-24 11:24:49 +0200

    created Reservation controller, reservation/index.html.twig

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ReservationController extends AbstractController
+{
+    /**
+     * @Route("/reservation", name="reservation")
+     */
+    public function index()
+    {
+        return $this->render('reservation/index.html.twig', [
+            'controller_name' => 'ReservationController',
+        ]);
+    }
+}
