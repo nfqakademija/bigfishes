9a0c576a41db963ce8b8eb3506570d8fba0054af
RitaJ <ritaj777@gmail.com>
2018-11-24 18:49:29 +0200

    build reservation form

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,18 +16,28 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dateFrom')
-            ->add('dateTo')
-            ->add('name')
-            ->add('fishersNumber')
-            ->add('house')
-            ->add('hours')
-            ->add('paymentStatus')
-            ->add('paymentId')
-            ->add('amount')
-            ->add('sectorName')
-            ->add('userId')
-            ->add('status')
+            ->add('name', TextType::class, array(
+                'label' => 'Reservation name',
+            ))
+            //->add('dateFrom', DateType::class)
+            ->add('dateTo', DateType::class, [
+                'required' => true,
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control input-inline datetimepicker',
+                    'data-provide' => 'datetimepicker',
+                    'format' => 'Y-m-d',
+                    'html5' => false,
+                ]])
+            ->add('fishersNumber', ChoiceType::class, array(
+                'choices'  => array(1 => 1, 2 => 2,),
+                'expanded' => true,
+                'data' => 1,
+            ))
+            //->add('hours')
+            //->add('amount')
+            //->add('sectorName')
+            ->add('userId', HiddenType::class)
         ;
     }
 
