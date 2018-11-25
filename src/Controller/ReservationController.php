0363d8286cd555ab9058a4e9ad9c4c3d03536caf
RitaJ <ritaj777@gmail.com>
2018-11-25 12:31:03 +0200

    rename twig reservation/create.html.twig

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -10,21 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationController extends AbstractController
 {
+    const SECTOR_NAME = "Trečias Sektorius";
+    const AMOUNT = 30;
+    const HOURS = 36;
+    const USER_ID = 5;
+    const STATUS = true;
     /**
-     * @Route("/reservation", name="reservation")
+     * @Route("/reservation", name="reservation_create")
      */
-    public function index(Request $request)
+    public function create(Request $request)
     {
         $dateFrom = new \DateTime('2018-11-27');
-        $sectorName = "Trečias Sektorius";
-        $amount = 30;
-        $house = 0;
-        $userId = 1;
-        $hours = 36;
-
-        if ($sectorName === "Trečias Sektorius") {
-            $house = 1;
-        }
+        $house = self::SECTOR_NAME === "Trečias Sektorius" ? true : false;
 
         $reservation = new Reservation();
         $form = $this->createForm(ReservationType::class, $reservation);
@@ -33,17 +30,18 @@ class ReservationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $timeFrom = $request->request->get('timeFrom');
             $reservation->setDateFrom($dateFrom->setTime($timeFrom, '00'));
+
             $dateTo = new \DateTime($request->request->get('reservation')['dateTo']);
             $timeTo = $request->request->get('timeTo');
             $reservation->setDateTo($dateTo->setTime($timeTo, '00'));
-            $reservation->setSectorName($sectorName);
-            $reservation->setHours($hours);
-            $reservation->setAmount($amount);
 
+            $reservation->setSectorName(self::SECTOR_NAME);
+            $reservation->setHours(self::HOURS);
+            $reservation->setAmount(self::AMOUNT);
             $reservation->setHouse($house);
             $reservation->setPaymentStatus('not paid');
-            $reservation->setUserId($userId);
-            $reservation->setStatus(1);
+            $reservation->setUserId(self::USER_ID);
+            $reservation->setStatus(self::STATUS);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($reservation);
@@ -54,12 +52,12 @@ class ReservationController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        return $this->render('reservation/index.html.twig', [
+        return $this->render('reservation/create.html.twig', [
             'form' => $form->createView(),
             'dateFrom' => $dateFrom->format('Y-m-d'),
-            'sectorNumber' => $sectorName,
-            'hours' => $hours,
-            'amount' => $amount,
+            'sectorNumber' => self::SECTOR_NAME,
+            'hours' => self::HOURS,
+            'amount' => self::AMOUNT,
             'house' => $house
         ]);
     }
