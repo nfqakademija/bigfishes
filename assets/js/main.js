6e2b8819f0402964c2ff48ed856d77a4e6825123
vic <viktoras.jefimovas@gmail.com>
2018-12-01 18:30:11 +0200

    new calendar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,98 +1,81 @@
-import Calendar from './class/Calendar.js';
-import { check_cell_status } from './calendar_editor.js';
+import {isWithinRange, isBefore, addDays, format} from 'date-fns';
+
+const jj = JSON.parse(JSON.parse(document.getElementsByClassName("json_info")[0].getAttribute("data-calendar_information")));
+console.log(jj);
+
+
+const STATUS_BUSY_FIRST = "busy";
+const STATUS_BUSY_SECOND = "busy-start_from_20";
+const STATUS_BUSY_THIRD = "free";
+const STATUS_BUSY_FOURTH = "busy-start_from_8";
+const STATUS_BUSY_FIFTH = "busy-start_from_8";
+const STATUS_BUSY = "busy";
+const STATUS_FREE = "free";
 
 
 
+const today = new Date();
+const dates = [];
 
-
-const ajax_call_response = '{"sector1":\n' +
-    '    {\n' +
-    '     "name": "Pirmas sektorius",\n' +
-    '     "reservation_dates":\n' +
-    '\t                      {"date0":\n' +
-    '\t\t                              {\n' +
-    '\t\t                                "start_date":"2018-11-01",\n' +
-    '         \t                         "start_from_8":true,\n' +
-    '\t                                  "start_from_20":false,\n' +
-    '                                    "user":"Dainius",\n' +
-    '\t                                  "reserved_till":"2018-12-02",\n' +
-    '        \t                          "end_to_8":true,\n' +
-    '        \t                          "end_to_20":false\n' +
-    '\t\t                              },\n' +
-    '                         "date1":\n' +
-    '\t\t                              {\n' +
-    '\t\t                                "start_date":"2018-12-05",\n' +
-    '\t                                  "start_from_8":false,\n' +
-    '\t                                  "start_from_20":true,\n' +
-    '        \t                          "user":"Laimutis",\n' +
-    '        \t                          "reserved_till":"2018-12-07",\n' +
-    '        \t                          "end_to_8":false,\n' +
-    '        \t                          "end_to_20":true\n' +
-    '                                   }\n' +
-    '\t                      }\n' +
-    '    },\n' +
-    ' "sector2":\n' +
-    '    {\n' +
-    '     "name": "Antras sektorius",\n' +
-    '     "reservation_dates": {} \n' +
-    '    },\n' +
-    ' "sector3":\n' +
-    '    {\n' +
-    '     "name": "Trecias sektorius",\n' +
-    '     "reservation_dates": {} \n' +
-    '    },\n' +
-    ' "sector4":\n' +
-    '    {\n' +
-    '     "name": "Ketvirtas sektorius",\n' +
-    '     "reservation_dates": \n' +
-    '                        {"date0":\n' +
-    '\t\t                              {\n' +
-    '\t\t                                "start_date":"2018-11-28",\n' +
-    '\t                                  "start_from_8":false,\n' +
-    '\t                                  "start_from_20":true,\n' +
-    '        \t                          "user":"Anzelmas",\n' +
-    '        \t                          "reserved_till":"2018-12-11",\n' +
-    '        \t                          "end_to_8":false,\n' +
-    '        \t                          "end_to_20":true\n' +
-    '                                   }\n' +
-    '                          \n' +
-    '                        } \n' +
-    '    },\n' +
-    ' "sector5":\n' +
-    '    {\n' +
-    '     "name": "Penktas sektorius",\n' +
-    '     "reservation_dates": {} \n' +
-    '    },\n' +
-    ' "sector6":\n' +
-    '    {\n' +
-    '     "name": "Sestas sektorius",\n' +
-    '     "reservation_dates": {} \n' +
-    '    },\n' +
-    ' "sector7":\n' +
-    '    {\n' +
-    '     "name": "Septintas sektorius",\n' +
-    '     "reservation_dates": {} \n' +
-    '    }\n' +
-    '}';
-const sectors_information = JSON.parse(ajax_call_response);
-
-
-const calendar = new Calendar(sectors_information);
-
-for (let i = 0; i < calendar.sector_dates.length; i++)
-{
-    $('.table_head_row').append('<th class="table_head_cell">'+new Date(calendar.sector_dates[i]).toISOString().slice(8, 10)+'</th>');
-}
-
-for (let i = 0; i < calendar.sector_names.length; i++) {
-    $('.calendar_table').append('<tr class="sectors" id="sector' + (i+1) + '">' + '<td class="sectors_cell">' + calendar.sector_names[i] + '</td></tr>');
+for (let day = new Date(); isBefore(day, addDays(today, 30)); day = addDays(day, 1)){
+    dates.push({'date': format(day, 'YYYY-MM-DD'),
+        'dayNumber': format(day, 'DD'),
+        'dayOfWeek': format(day, 'ddd')
+    });
 }
 
 
-for (const sector of Object.keys(calendar.sectors)) {
-    for (const cell of Object.keys(calendar.sectors[sector].cells))
-        $('#'+[sector]).append('<td class="sectors_day_cell '+check_cell_status((calendar.sectors[sector].cells[cell]))+'"><a href="/reservation?date='+calendar.sectors[sector].cells[cell].date+'&sector_name='+calendar.sectors[sector].name+'" style="display:block;">&nbsp;</a></td>');
+for (let i = 0; i < dates.length; i++) {
+    $('.table_head_row').append('<th class="table_head_cell '+dates[i].dayOfWeek+'">' + dates[i].dayNumber + '</th>');
 }
+
+
+for (const sector in jj) {
+    $('.calendar_table').append('<tr class="sectors" id="'+sector+'">' +
+        '<td class="sectors_cell">' + jj[sector].name + '</td>');
+    for (let i = 0; i < dates.length; i++) {
+        let dayInfo = STATUS_FREE;
+        let name = ""
+        for (const reservationId in jj[sector].reservation_dates) {
+
+            const reservation = jj[sector].reservation_dates[reservationId];
+            const isInRange = isWithinRange(dates[i].date, reservation.dateFrom, reservation.dateTo);
+
+
+            if (isInRange && dates[i].date === reservation.dateFrom && reservation.timeFrom === '08' && reservation.dateFrom !== reservation.dateTo){
+                dayInfo = STATUS_BUSY_FIRST;
+                name = reservation.name;
+            }
+            else if (isInRange && dates[i].date === reservation.dateFrom && reservation.timeFrom === '20'){
+                dayInfo = STATUS_BUSY_SECOND;
+                name = reservation.name;
+            }
+            else if (isInRange && dates[i].date === reservation.dateTo && reservation.timeTo === '08'){
+                dayInfo = STATUS_BUSY_THIRD;
+                name = reservation.name;
+            }
+            else if (isInRange && dates[i].date === reservation.dateTo && reservation.timeTo === '20'){
+                dayInfo = STATUS_BUSY_FOURTH;
+                name = reservation.name;
+            }
+            else if (isInRange && reservation.dateFrom === reservation.dateTo && reservation.timeFrom === '08' && reservation.timeTo === '20'){
+                dayInfo = STATUS_BUSY_FIFTH;
+                name = reservation.name;
+            }
+            else if (isInRange && dates[i].date !== reservation.dateFrom && dates[i].date !== reservation.dateTo){
+                dayInfo = STATUS_BUSY;
+                name = reservation.name;
+            }
+
+        }
+        $('#'+[sector]).append('<td class="sectors_day_cell '+dayInfo+' '+dates[i].dayOfWeek+'" title="'+name+'"><a href="/reservation?date='+dates[i].date+'&sector_name='+jj[sector].name+'" >&nbsp;</a></td>');
+
+    }
+}
+
+
+
+
 
 
 
