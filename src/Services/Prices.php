d89a041569b6b05972768bf5cd80dc959f21254c
RitaJ <ritaj777@gmail.com>
2018-12-07 10:46:26 +0200

    Created Prices Service for prices calculation

--- src/Services/Prices.php
+++ src/Services/Prices.php
@@ -6,9 +6,27 @@ namespace App\Services;
 
 class Prices
 {
-    const PRICE_12 = 10;
-    public function reservationPriceCalculation()
-    {
+    const PRICE_FISHING_12_h = 10;
+    const PRICE_HOUSE_12_h = 10;
+    const DISCOUNT = 0.9;
 
+    public function fishingPriceCalculation($fishersNumber, $hours)
+    {
+        if ($fishersNumber === 1) {
+            return $fishersNumber * ($hours/12 * self::PRICE_FISHING_12_h);
+        } elseif ($fishersNumber === 2) {
+            return $fishersNumber * ($hours / 12 * self::PRICE_FISHING_12_h * self::DISCOUNT);
+        }
+        return false;
+    }
+
+    public function housePriceCalculation($hours)
+    {
+        return $hours/12 * self::PRICE_HOUSE_12_h;
+    }
+
+    public function totalPriceCalculation($priceFirst, $priceSecond)
+    {
+        return $priceFirst + $priceSecond;
     }
 }
