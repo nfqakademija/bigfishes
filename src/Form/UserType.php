edad0ce473c5d0a0766c00ba9159bbcdefd96c59
RitaJ <ritaj777@gmail.com>
2018-12-15 19:50:32 +0200

    validation messages text correction

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,13 +8,14 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', null, array('label' => false))
+            ->add('name', TextType::class, array('label' => false ))
             ->add('email', EmailType::class, array('label' => false))
             ->add('password', PasswordType::class, array('label' => false))
         ;
