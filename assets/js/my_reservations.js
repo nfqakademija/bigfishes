8ad160f018554d1ec5ba0e1da8df136f8b6e3f4c
vic <viktoras.jefimovas@gmail.com>
2018-12-18 23:03:37 +0200

    new design

--- assets/js/my_reservations.js
+++ assets/js/my_reservations.js
@@ -1,23 +1,23 @@
 const user_reservations = JSON.parse(user_data.replace(/&quot;/g,'"'));
-const table_head_names = ['Rez. pavadinimas','Nuo', 'Iki', 'Sektorius', 'Žuklės val.', 'Žvejų sk.', 'Kaina', 'Apmokėta'];
+const table_head_names = ['','Rez. pavadinimas','Nuo', 'Iki', 'Sektorius', 'Žuklės val.', 'Žvejų sk.', 'Kaina', 'Apmokėta'];
 
 if (user_reservations.length){
     for (let i = 0; i < table_head_names.length; i++){
-        $('.ur_head').append('<th class="ur_table_head_cell">'+table_head_names[i]+'</th>');
+        $('.ur_table_head_row').append('<th class="align-middle my_reservations_th-'+i+'">'+table_head_names[i]+'</th>');
     }
 
 
     for (let i = 0; i < user_reservations.length; i++){
-        $('.ur_table').append('<tr>' +
-            '<td class="ur_table_cell">'+(i+1)+'</td>'+
+        $('.tbody').append('<tr>' +
+            '<td class="text-center">'+(i+1)+'</td>'+
             '<td class="ur_table_cell">'+user_reservations[i].reservation_name+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].dateFrom+' '+user_reservations[i].timeFrom+':00'+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].dateTo+' '+user_reservations[i].timeTo+':00'+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].sectorName+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].hours+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].fishersNumber+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].amount+'</td>'+
-            '<td class="ur_table_cell">'+user_reservations[i].paymentStatus+'</td>'+
+            '<td class="text-center">'+user_reservations[i].dateFrom+' '+user_reservations[i].timeFrom+':00'+'</td>'+
+            '<td class="text-center">'+user_reservations[i].dateTo+' '+user_reservations[i].timeTo+':00'+'</td>'+
+            '<td class="text-center">'+user_reservations[i].sectorName+'</td>'+
+            '<td class="text-center">'+user_reservations[i].hours+'</td>'+
+            '<td class="text-center">'+user_reservations[i].fishersNumber+'</td>'+
+            '<td class="text-center">'+user_reservations[i].amount+'</td>'+
+            '<td class="text-center">'+user_reservations[i].paymentStatus+'</td>'+
             '</tr>')
     }
 }
