7ccc814f826e6a5f90ad4c0e11c8eb66b8893b6f
vic <viktoras.jefimovas@gmail.com>
2018-12-18 22:26:38 +0200

    prices added

--- assets/js/reservations.js
+++ assets/js/reservations.js
@@ -5,25 +5,6 @@ if ($(window).width() < 600) {
     $('.left_button-back').text('').append('<i class="fa fa-angle-double-left" style="color: white; font-size: 1em"></i>');
 }
 
-if (datePressed == date_from_BUSY && time_from_BUSY === '20') {
-        $('input#reservation_timeFrom_1').attr('disabled', true);
-        $('input#reservation_timeTo_0').attr('disabled', true);
-        $('input#reservation_timeFrom_0').attr('checked', 'checked');
-        $('input#reservation_timeTo_1').attr('checked', 'checked');
-}
-
-if(!isFromAvailableFrom8){
-    $('input#reservation_timeFrom_0').attr({'disabled': true, 'checked': ''});
-    $('input#reservation_timeFrom_1').attr('checked', 'checked');
-}
-
-
-
-
-
-
-
-
 if ($('.wrong_sector_title')[0].innerHTML && $('.wrong_sector_title')[0].innerHTML === 'Blogai pasirinktas sektorius') {
     $('form').remove();
     $('.reservation_form .box-success').append('<div>' +
